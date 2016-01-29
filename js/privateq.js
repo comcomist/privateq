77559bf52ae42ddbcce5b9ff87c2558e1b41cb27
comcomist <namzezam@riseup.net>
2016-01-29 15:57:25 +0100

    update

--- js/privateq.js
+++ js/privateq.js
@@ -45,10 +45,19 @@ return	base16to32of64(CryptoJS.SHA256(data).toString());
 //String.fromCharCode.apply(null, new Uint16Array(data));
 
 
-	 document.getElementById('ioput').value=data;//str;//window.content;//data;
+	 document.getElementById('ioput').value=data;
 	 }
 
  //----------------------ViewTable
+/* to locate the row on line and colm on skip comma on the ioput
+function clicked() {//http://stackoverflow.com/questions/28357314/find-row-and-column-of-clicked-table-cell-in-javascript-without-jquery
+    alert("clicked cell at: " + this.cellIndex + ", " + this.parentNode.rowIndex);
+}*/
+function onc(i) {var t = prompt( "Press ok to replace the value: " + i.textContent  , i.textContent);//("+i.parentNode.rowIndex +","+ i.cellIndex +")
+ if (t != null)  i.textContent=t;}
+ 
+function export2csv() {$('#table-container').table2CSV();}//	var t = prompt( "Press ok to" , "123i.textContent");}
+
 // $(".container-fluid .var_app").html(); // Using the jQuery library 
  function init_table(data) {// Thanks to Derek Eder under mit: Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	// data=ab2str(data);
@@ -65,49 +74,56 @@ return	base16to32of64(CryptoJS.SHA256(data).toString());
   var allow_download = options.allow_download || false;
   var csv_options = options.csv_options || {};
   var datatables_options = options.datatables_options || {};
-
-  $("#" + el).html("<table class='table table-striped table-condensed' id='my-table'></table>");
+  //var bsave='<button onclick="saveioput this( document.getElementById('ioput').value  );"' ;
+  //+'<button onclick="saveioput();">Save</button>'
+//  '<button onclick="export2csv();">Update</button>'+
+  $("#" + el).html('<button onclick="export2csv();">Update</button>'+'<button onclick="saveioput();">Save</button>'+"<table class='table table-striped table-condensed' id='my-table'></table>");
 
   //$.when($.get(csv_path)).then(function(data){
 
       data = data.replace(/[\r|\r\n]/g, "\n");
       
       var csv_data = $.csv.toArrays(data, csv_options);
-
-      var table_head = "<thead><tr>";
+      
+      var   table_head=  "<thead><tr>";
 
       for (head_id = 0; head_id < csv_data[0].length; head_id++) { 
-        table_head += "<th>" + csv_data[0][head_id] + "</th>";
+		table_head += "<th>" + csv_data[0][head_id] + "</th>";
+ 
       }
 
       table_head += "</tr></thead>";
+     
       $('#my-table').append(table_head);
+     
       $('#my-table').append("<tbody></tbody>");
 
       for (row_id = 1; row_id < csv_data.length; row_id++) { 
         var row_html = "<tr>";
 
           for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
-            row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
+            row_html += "<td  onclick='onc(this)'>" + csv_data[row_id][col_id] + "</td>";
           }
           
         row_html += "</tr>";
+
         $('#my-table tbody').append(row_html);
       }
 
-      $("#my-table").DataTable(datatables_options);
+      $("#my-table").DataTable(datatables_options);// sorting it all
+
 	  //document.getElementById('ioput').value=null;
      // if (allow_download) $("#" + el).append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
     //    });
 }
-///
+
 function clearvar(){ // why it is not worling?
 	  window.content=null;
 	 // window.Scontent="";
 	 
     window.hname=null;
     document.getElementById('ioput').value= "";//null;   // document.getElementById('harea').value= null;
-	init_table(window.content);}
+	init_table(null);}
 //---------------------------OpenFile 
   //http://www.html5rocks.com/en/tutorials/file/dndfiles/
   var reader;
@@ -130,6 +146,7 @@ function clearvar(){ // why it is not worling?
         alert('An error occurred reading this file.');
     };
   }
+
 
   function updateProgress(evt) {
     
@@ -207,9 +224,12 @@ function clearvar(){ // why it is not worling?
 
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
 //--------------------------savefile
-function savefile(fileName){ // saveas http://stackoverflow.com/questions/28263221/save-html-textarea-state
+function saveioput(){savefile('nameit.csv', document.getElementById('ioput').value);}
+
+function savefile(fileName,content){ // saveas http://stackoverflow.com/questions/28263221/save-html-textarea-state
 //var ToWrite =window.content ; //document.getElementById('ioput').value;
-    var blobas = new Blob([window.content]);//[ToWrite]);//, {type:'text/plain'});
+    //var blobas = new Blob([window.content]);//[ToWrite]);//, {type:'text/plain'});
+     var blobas = new Blob([content]);//[ToWrite]);//, {type:'text/plain'});
     var saveas = document.createElement("a");
     saveas.download = fileName;
     saveas.innerHTML = "Download File";
